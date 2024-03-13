531841133d0ac15af10e75853b8438f0a871b08f
SENC2023 <145979246+SENC2023@users.noreply.github.com>
2024-03-13 10:47:50 +0000

    Creación para Fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,101 @@
-import React from "react";
+// import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// //create your first component
+// const Home = () => {
+	
+// 	const [inputValue, setInputValue] = useState("")
+// 	const [todos, setTodos] = useState([])
 
-//create your first component
+// 	console.log(inputValue);
+
+// 	return (
+// 		<div className="container">
+// 			<h1>Lista de tareas</h1>
+// 			<ul>
+// 				<li>
+// 					<input 
+// 					type="text" 
+// 					onChange={(llamar) => setInputValue(llamar.target.value)}
+// 					value={inputValue} 
+// 					onKeyDown={(enter) => {
+// 						console.log("ENTER");
+// 						if (enter.key === "Enter") {
+// 							setTodos(todos.concat(inputValue))
+// 							setInputValue("")
+// 						}
+// 					}}
+// 					placeholder="Prueba" />
+// 				</li>
+// 				{todos.map((item, index) => ( 
+// 					<li>
+// 						{item}
+// 					</li>
+// 				))}
+// 				<button className="btn btn-primary" 
+// 					onClick={() => 
+// 								setTodos(
+// 									todos.filter(
+// 										(t, currentIndex) =>
+// 											index != currentIndex
+// 									)
+// 								)
+// 							}>DELETE ME</button>
+// 			</ul>
+// 			<div>
+// 				89 cosas por hacer
+// 			</div>
+// 		</div>
+// 	);
+// };
+
+// export default Home;
+
+import React, { useState } from "react";
+
+// create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <div className="container">
+      <h1>Lista de tareas</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            onChange={(llamar) => setInputValue(llamar.target.value)}
+            value={inputValue}
+            onKeyDown={(enter) => {
+              if (enter.key === "Enter") {
+                setTodos([...todos, inputValue]);
+                setInputValue("");
+              }
+            }}
+            placeholder="Prueba"
+          />
+        </li>
+        {todos.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button
+              className="btn btn-primary"
+              onClick={() =>
+                setTodos(
+                  todos.filter(
+                    (t, currentIndex) => index !== currentIndex
+                  )
+                )
+              }
+            >
+              DELETE ME
+            </button>
+          </li>
+        ))}
+      </ul>
+      <div>{todos.length} cosas por hacer</div>
+    </div>
+  );
 };
 
 export default Home;
