f735598a24be082dec67fae43fdcc0e99ee9fcb4
SENC2023 <145979246+SENC2023@users.noreply.github.com>
2024-03-18 21:59:36 +0000

    Terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,99 +1,48 @@
-// import React, {useState} from "react";
-
-// //create your first component
-// const Home = () => {
-	
-// 	const [inputValue, setInputValue] = useState("")
-// 	const [todos, setTodos] = useState([])
-
-// 	console.log(inputValue);
-
-// 	return (
-// 		<div className="container">
-// 			<h1>Lista de tareas</h1>
-// 			<ul>
-// 				<li>
-// 					<input 
-// 					type="text" 
-// 					onChange={(llamar) => setInputValue(llamar.target.value)}
-// 					value={inputValue} 
-// 					onKeyDown={(enter) => {
-// 						console.log("ENTER");
-// 						if (enter.key === "Enter") {
-// 							setTodos(todos.concat(inputValue))
-// 							setInputValue("")
-// 						}
-// 					}}
-// 					placeholder="Prueba" />
-// 				</li>
-// 				{todos.map((item, index) => ( 
-// 					<li>
-// 						{item}
-// 					</li>
-// 				))}
-// 				<button className="btn btn-primary" 
-// 					onClick={() => 
-// 								setTodos(
-// 									todos.filter(
-// 										(t, currentIndex) =>
-// 											index != currentIndex
-// 									)
-// 								)
-// 							}>DELETE ME</button>
-// 			</ul>
-// 			<div>
-// 				89 cosas por hacer
-// 			</div>
-// 		</div>
-// 	);
-// };
-
-// export default Home;
-
 import React, { useState } from "react";
 
-// create your first component
 const Home = () => {
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
 
+function addTodos(enter) {
+    if (enter.key === "Enter") {
+      setTodos(todos.concat( { label: inputValue, done: false }))
+      setInputValue("");
+    }
+}
+
+function removeTodos(currentIndex) {
+   const filter = todos.filter((item, index) => index !== currentIndex)
+   setTodos(filter)
+}
+
+console.log(todos)
+
   return (
     <div className="container">
-      <h1>Lista de tareas</h1>
+      <h1 className="text-center">Lista de tareas</h1>
       <ul>
         <li>
           <input
             type="text"
             onChange={(llamar) => setInputValue(llamar.target.value)}
             value={inputValue}
-            onKeyDown={(enter) => {
-              if (enter.key === "Enter") {
-                setTodos([...todos, inputValue]);
-                setInputValue("");
-              }
-            }}
-            placeholder="Prueba"
+            onKeyDown={addTodos}
+            placeholder="Agrega una nueva tarea"
           />
         </li>
         {todos.map((item, index) => (
           <li key={index}>
-            {item}
-            <button
-              className="btn btn-primary"
-              onClick={() =>
-                setTodos(
-                  todos.filter(
-                    (t, currentIndex) => index !== currentIndex
-                  )
-                )
-              }
-            >
-              DELETE ME
+            {item.label}
+            <button 
+              
+              onClick={() => removeTodos(index)}>
+              <i className="fa-solid fa-xmark"></i>
             </button>
           </li>
         ))}
       </ul>
-      <div>{todos.length} cosas por hacer</div>
+      <div>Tienes {todos.length} tareas pendientes por hacer</div>
     </div>
   );
 };
